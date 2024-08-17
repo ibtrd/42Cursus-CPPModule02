3daf73b3e2e36bd4c5375e595680d79e0c457b0f
ibertran <128724498+ibtrd@users.noreply.github.com>
2024-08-17 22:56:57 +0200

    ex01: Refacto

--- ex01/srcs/Fixed.cpp
+++ ex01/srcs/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: ibertran <ibertran@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 15:16:07 by ibertran          #+#    #+#             */
-/*   Updated: 2024/07/29 18:02:07 by ibertran         ###   ########lyon.fr   */
+/*   Updated: 2024/08/17 22:32:13 by ibertran         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,9 @@
 
 const int	Fixed::_fractionalBits = 8;
 
-/* ORTHODOX CANONICAL FORM ************************************************** */
+/* CONSTRUCTORS ************************************************************* */
 
-Fixed::Fixed(void)
-: _value(0)
+Fixed::Fixed(void) : _value(0)
 {
 	std::cout << "Default constructor called" << std::endl;
 }
@@ -31,6 +30,25 @@ Fixed::Fixed(const Fixed &copy)
 	*this = copy;
 }
 
+Fixed::Fixed(const int n) : _value(n << _fractionalBits)
+{
+	std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(const float n) : _value(roundf(n * (1 << _fractionalBits)))
+{
+	std::cout << "Float constructor called" << std::endl;
+}
+
+/* ************************************************************************** */
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+/* ************************************************************************** */
+
 Fixed &Fixed::operator=(const Fixed &other)
 {
 	std::cout << "Copy assignment operator called" << std::endl;
@@ -38,11 +56,6 @@ Fixed &Fixed::operator=(const Fixed &other)
 		return (*this);
 	this->setRawBits(other.getRawBits());
 	return (*this);
-}
-
-Fixed::~Fixed(void)
-{
-	std::cout << "Destructor called" << std::endl;
 }
 
 /* ************************************************************************** */
@@ -53,17 +66,7 @@ std::ostream	&operator<<(std::ostream &os, const Fixed &ref)
 	return (os);	
 }
 
-Fixed::Fixed(const int n)
-: _value(n << _fractionalBits)
-{
-	std::cout << "Int constructor called" << std::endl;
-}
-
-Fixed::Fixed(const float n)
-: _value(roundf(n * (1 << _fractionalBits)))
-{
-	std::cout << "Float constructor called" << std::endl;
-}
+/* ************************************************************************** */
 
 int		Fixed::getRawBits(void) const
 { 
